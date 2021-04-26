904559c84bea334d88fd27faf3954e8a6794a48a
Manda Snehashish Reddy <ashishreddy9000@gmail.com>
2021-04-27 01:17:57 +0530

    Add Cell Flip Functionality

--- src/Board.tsx
+++ src/Board.tsx
@@ -23,13 +23,39 @@ const Board: React.FC<BoardProps> = (props) => {
 
   let [board, setBoard] = React.useState(createBoard());
 
+  const flipCellsAroundIt = (c: string): void => {
+    let temporaryBoard: boolean[][] = [...board];
+    let [y, x] = c.split("-").map(Number);
+
+    const flipCell = (y: number, x: number): void => {
+      if (x >= 0 && x < props.ncols && y >= 0 && y < props.nrows) {
+        temporaryBoard[y][x] = !temporaryBoard[y][x];
+      }
+    };
+
+    flipCell(y, x);
+
+    flipCell(y + 1, x);
+    flipCell(y - 1, x);
+    flipCell(y, x + 1);
+    flipCell(y, x - 1);
+
+    setBoard(temporaryBoard);
+  };
+
   const renderBoard = (): JSX.Element[] => {
     let tableBoard: JSX.Element[] = [];
     for (let y: number = 0; y < props.nrows; y++) {
       let row: JSX.Element[] = [];
       for (let x: number = 0; x < props.ncols; x++) {
         let c = `${y}-${x}`;
-        row.push(<Cell key={c} isLit={board[y][x]} />);
+        row.push(
+          <Cell
+            key={c}
+            isLit={board[y][x]}
+            flipCellsAround={() => flipCellsAroundIt(c)}
+          />
+        );
       }
       tableBoard.push(<tr key={y}>{row}</tr>);
     }
--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { MouseEventHandler } from "react";
 import "./Cell.scss";
 
 interface CellProps {
   key: string;
   isLit: boolean;
+  flipCellsAround(): void;
 }
 
 const Cell: React.FC<CellProps> = (props) => {
+  const handleClick = (evt: any): void => {
+    props.flipCellsAround();
+  };
   let isLitClass: string = "Cell " + (props.isLit ? "Lit" : "");
 
-  return <td className={isLitClass} />;
+  return <td className={isLitClass} onClick={handleClick} />;
 };
 
 export default Cell;
